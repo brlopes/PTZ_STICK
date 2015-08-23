22c6bdeb4e403a6730f2cffb5a6ad067b5dec011
brlopes <rl.bruno@gmail.com>
2015-08-23 08:55:13 +0000

    added blog for logging progress

---
+++ build/micawber/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='micawber',
+    version='0.3.3',
+    description='a small library for extracting rich content from urls',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/micawber/',
+    packages=find_packages(),
+    package_data = {
+        'micawber': [
+            'contrib/mcdjango/templates/micawber/*.html',
+        ],
+        'examples': [
+            #'requirements.txt',
+            '*/static/*.css',
+            '*/templates/*.html',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    test_suite='runtests.runtests',
+)
